0a6d14efc17d1c0bf2d8a2d1c824cdd6cd7173db
LUIS CARLOS MURILLO ROSALES <lcmurillor@est.utn.ac.cr>
2019-07-23 23:09:40 -0600

    Ejemplo1

--- Proyecto Sistema Bolpa/Formularios/FrmArticulo.vb
+++ Proyecto Sistema Bolpa/Formularios/FrmArticulo.vb
@@ -6,4 +6,9 @@
 		' Agregue cualquier inicialización después de la llamada a InitializeComponent().
 	End Sub
 
+    'Luis Carlos agregó un comentario a este proyecto
+
+    Private Sub FrmArticulo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
