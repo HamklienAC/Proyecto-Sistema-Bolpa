29dbce459003fb754a21dee5a21d5f1ff2f1bdfd
HamklienAC <49457002+HamklienAC@users.noreply.github.com>
2019-07-24 14:45:12 -0600

    Flexibilidad en opciones de agregar familias y subfamilias

--- Proyecto Sistema Bolpa/Formularios/FrmArticulo.Designer.vb
+++ Proyecto Sistema Bolpa/Formularios/FrmArticulo.Designer.vb
@@ -29,53 +29,54 @@ Partial Class FrmArticulo
 		Me.TextBox5 = New System.Windows.Forms.TextBox()
 		Me.TextBox6 = New System.Windows.Forms.TextBox()
 		Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-		Me.TextBox7 = New System.Windows.Forms.TextBox()
-		Me.TextBox8 = New System.Windows.Forms.TextBox()
-		Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-		Me.ComboBox2 = New System.Windows.Forms.ComboBox()
-		Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+		Me.txtNuevaSubfamilia = New System.Windows.Forms.TextBox()
+		Me.txtNuevaFamilia = New System.Windows.Forms.TextBox()
+		Me.cbFamilia = New System.Windows.Forms.ComboBox()
+		Me.cbSubfamilia = New System.Windows.Forms.ComboBox()
+		Me.cbAgregarFam = New System.Windows.Forms.CheckBox()
+		Me.cbAgregarSub = New System.Windows.Forms.CheckBox()
 		Me.SuspendLayout()
 		'
 		'TextBox1
 		'
 		Me.TextBox1.Location = New System.Drawing.Point(13, 13)
 		Me.TextBox1.Name = "TextBox1"
-		Me.TextBox1.Size = New System.Drawing.Size(183, 20)
+		Me.TextBox1.Size = New System.Drawing.Size(199, 20)
 		Me.TextBox1.TabIndex = 0
 		'
 		'TextBox2
 		'
 		Me.TextBox2.Location = New System.Drawing.Point(13, 39)
 		Me.TextBox2.Name = "TextBox2"
-		Me.TextBox2.Size = New System.Drawing.Size(183, 20)
+		Me.TextBox2.Size = New System.Drawing.Size(199, 20)
 		Me.TextBox2.TabIndex = 1
 		'
 		'TextBox3
 		'
 		Me.TextBox3.Location = New System.Drawing.Point(12, 65)
 		Me.TextBox3.Name = "TextBox3"
-		Me.TextBox3.Size = New System.Drawing.Size(183, 20)
+		Me.TextBox3.Size = New System.Drawing.Size(200, 20)
 		Me.TextBox3.TabIndex = 2
 		'
 		'TextBox4
 		'
 		Me.TextBox4.Location = New System.Drawing.Point(12, 91)
 		Me.TextBox4.Name = "TextBox4"
-		Me.TextBox4.Size = New System.Drawing.Size(183, 20)
+		Me.TextBox4.Size = New System.Drawing.Size(200, 20)
 		Me.TextBox4.TabIndex = 3
 		'
 		'TextBox5
 		'
 		Me.TextBox5.Location = New System.Drawing.Point(12, 117)
 		Me.TextBox5.Name = "TextBox5"
-		Me.TextBox5.Size = New System.Drawing.Size(183, 20)
+		Me.TextBox5.Size = New System.Drawing.Size(200, 20)
 		Me.TextBox5.TabIndex = 4
 		'
 		'TextBox6
 		'
 		Me.TextBox6.Location = New System.Drawing.Point(12, 170)
 		Me.TextBox6.Name = "TextBox6"
-		Me.TextBox6.Size = New System.Drawing.Size(183, 20)
+		Me.TextBox6.Size = New System.Drawing.Size(200, 20)
 		Me.TextBox6.TabIndex = 5
 		'
 		'DateTimePicker1
@@ -85,57 +86,69 @@ Partial Class FrmArticulo
 		Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
 		Me.DateTimePicker1.TabIndex = 6
 		'
-		'TextBox7
+		'txtNuevaSubfamilia
 		'
-		Me.TextBox7.Location = New System.Drawing.Point(13, 306)
-		Me.TextBox7.Name = "TextBox7"
-		Me.TextBox7.Size = New System.Drawing.Size(183, 20)
-		Me.TextBox7.TabIndex = 7
+		Me.txtNuevaSubfamilia.Enabled = False
+		Me.txtNuevaSubfamilia.Location = New System.Drawing.Point(13, 326)
+		Me.txtNuevaSubfamilia.Name = "txtNuevaSubfamilia"
+		Me.txtNuevaSubfamilia.Size = New System.Drawing.Size(201, 20)
+		Me.txtNuevaSubfamilia.TabIndex = 7
 		'
-		'TextBox8
+		'txtNuevaFamilia
 		'
-		Me.TextBox8.Location = New System.Drawing.Point(12, 280)
-		Me.TextBox8.Name = "TextBox8"
-		Me.TextBox8.Size = New System.Drawing.Size(183, 20)
-		Me.TextBox8.TabIndex = 8
+		Me.txtNuevaFamilia.Enabled = False
+		Me.txtNuevaFamilia.Location = New System.Drawing.Point(13, 273)
+		Me.txtNuevaFamilia.Name = "txtNuevaFamilia"
+		Me.txtNuevaFamilia.Size = New System.Drawing.Size(200, 20)
+		Me.txtNuevaFamilia.TabIndex = 8
 		'
-		'ComboBox1
+		'cbFamilia
 		'
-		Me.ComboBox1.FormattingEnabled = True
-		Me.ComboBox1.Location = New System.Drawing.Point(12, 196)
-		Me.ComboBox1.Name = "ComboBox1"
-		Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
-		Me.ComboBox1.TabIndex = 9
+		Me.cbFamilia.FormattingEnabled = True
+		Me.cbFamilia.Location = New System.Drawing.Point(12, 196)
+		Me.cbFamilia.Name = "cbFamilia"
+		Me.cbFamilia.Size = New System.Drawing.Size(200, 21)
+		Me.cbFamilia.TabIndex = 9
 		'
-		'ComboBox2
+		'cbSubfamilia
 		'
-		Me.ComboBox2.FormattingEnabled = True
-		Me.ComboBox2.Location = New System.Drawing.Point(13, 223)
-		Me.ComboBox2.Name = "ComboBox2"
-		Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
-		Me.ComboBox2.TabIndex = 10
+		Me.cbSubfamilia.FormattingEnabled = True
+		Me.cbSubfamilia.Location = New System.Drawing.Point(13, 223)
+		Me.cbSubfamilia.Name = "cbSubfamilia"
+		Me.cbSubfamilia.Size = New System.Drawing.Size(199, 21)
+		Me.cbSubfamilia.TabIndex = 10
 		'
-		'RadioButton1
+		'cbAgregarFam
 		'
-		Me.RadioButton1.AutoSize = True
-		Me.RadioButton1.Location = New System.Drawing.Point(13, 257)
-		Me.RadioButton1.Name = "RadioButton1"
-		Me.RadioButton1.Size = New System.Drawing.Size(90, 17)
-		Me.RadioButton1.TabIndex = 11
-		Me.RadioButton1.TabStop = True
-		Me.RadioButton1.Text = "RadioButton1"
-		Me.RadioButton1.UseVisualStyleBackColor = True
+		Me.cbAgregarFam.AutoSize = True
+		Me.cbAgregarFam.Location = New System.Drawing.Point(13, 250)
+		Me.cbAgregarFam.Name = "cbAgregarFam"
+		Me.cbAgregarFam.Size = New System.Drawing.Size(152, 17)
+		Me.cbAgregarFam.TabIndex = 12
+		Me.cbAgregarFam.Text = "Agregar una nueva familia "
+		Me.cbAgregarFam.UseVisualStyleBackColor = True
+		'
+		'cbAgregarSub
+		'
+		Me.cbAgregarSub.AutoSize = True
+		Me.cbAgregarSub.Location = New System.Drawing.Point(13, 303)
+		Me.cbAgregarSub.Name = "cbAgregarSub"
+		Me.cbAgregarSub.Size = New System.Drawing.Size(166, 17)
+		Me.cbAgregarSub.TabIndex = 13
+		Me.cbAgregarSub.Text = "Agregar una nueva subfamilia"
+		Me.cbAgregarSub.UseVisualStyleBackColor = True
 		'
 		'FrmArticulo
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(1151, 643)
-		Me.Controls.Add(Me.RadioButton1)
-		Me.Controls.Add(Me.ComboBox2)
-		Me.Controls.Add(Me.ComboBox1)
-		Me.Controls.Add(Me.TextBox8)
-		Me.Controls.Add(Me.TextBox7)
+		Me.ClientSize = New System.Drawing.Size(843, 643)
+		Me.Controls.Add(Me.cbAgregarSub)
+		Me.Controls.Add(Me.cbAgregarFam)
+		Me.Controls.Add(Me.cbSubfamilia)
+		Me.Controls.Add(Me.cbFamilia)
+		Me.Controls.Add(Me.txtNuevaFamilia)
+		Me.Controls.Add(Me.txtNuevaSubfamilia)
 		Me.Controls.Add(Me.DateTimePicker1)
 		Me.Controls.Add(Me.TextBox6)
 		Me.Controls.Add(Me.TextBox5)
@@ -157,9 +170,10 @@ Partial Class FrmArticulo
 	Friend WithEvents TextBox5 As TextBox
 	Friend WithEvents TextBox6 As TextBox
 	Friend WithEvents DateTimePicker1 As DateTimePicker
-	Friend WithEvents TextBox7 As TextBox
-	Friend WithEvents TextBox8 As TextBox
-	Friend WithEvents ComboBox1 As ComboBox
-	Friend WithEvents ComboBox2 As ComboBox
-	Friend WithEvents RadioButton1 As RadioButton
+	Friend WithEvents txtNuevaSubfamilia As TextBox
+	Friend WithEvents txtNuevaFamilia As TextBox
+	Friend WithEvents cbFamilia As ComboBox
+	Friend WithEvents cbSubfamilia As ComboBox
+	Friend WithEvents cbAgregarFam As CheckBox
+	Friend WithEvents cbAgregarSub As CheckBox
 End Class
--- Proyecto Sistema Bolpa/Formularios/FrmArticulo.vb
+++ Proyecto Sistema Bolpa/Formularios/FrmArticulo.vb
@@ -6,5 +6,24 @@
 		' Agregue cualquier inicialización después de la llamada a InitializeComponent().
 	End Sub
 
+	Private Sub CbAgregarFam_CheckedChanged(sender As Object, e As EventArgs) Handles cbAgregarFam.CheckedChanged
+		If cbAgregarFam.Checked Then
+			cbFamilia.Enabled = False
+			txtNuevaFamilia.Enabled = True
+		Else
+			cbFamilia.Enabled = True
+			txtNuevaFamilia.Enabled = False
+		End If
+	End Sub
+
+	Private Sub CbAgregarSub_CheckedChanged(sender As Object, e As EventArgs) Handles cbAgregarSub.CheckedChanged
+		If cbAgregarSub.Checked Then
+			cbSubfamilia.Enabled = False
+			txtNuevaSubfamilia.Enabled = True
+		Else
+			cbSubfamilia.Enabled = True
+			txtNuevaSubfamilia.Enabled = False
+		End If
+	End Sub
 
 End Class
