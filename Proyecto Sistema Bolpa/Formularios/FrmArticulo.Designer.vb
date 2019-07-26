9608d9dddd1f79fa50d0acce39f341eebc059ef8
HamklienAC <49457002+HamklienAC@users.noreply.github.com>
2019-07-26 16:01:55 -0600

    Filtros de escritura

--- Proyecto Sistema Bolpa/Formularios/FrmArticulo.Designer.vb
+++ Proyecto Sistema Bolpa/Formularios/FrmArticulo.Designer.vb
@@ -22,13 +22,13 @@ Partial Class FrmArticulo
 	'No lo modifique con el editor de código.
 	<System.Diagnostics.DebuggerStepThrough()>
 	Private Sub InitializeComponent()
-		Me.TextBox1 = New System.Windows.Forms.TextBox()
-		Me.TextBox2 = New System.Windows.Forms.TextBox()
-		Me.TextBox3 = New System.Windows.Forms.TextBox()
-		Me.TextBox4 = New System.Windows.Forms.TextBox()
-		Me.TextBox5 = New System.Windows.Forms.TextBox()
-		Me.TextBox6 = New System.Windows.Forms.TextBox()
-		Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+		Me.txtNombre = New System.Windows.Forms.TextBox()
+		Me.txtProveedor = New System.Windows.Forms.TextBox()
+		Me.txtCodigo = New System.Windows.Forms.TextBox()
+		Me.txtDescripcion = New System.Windows.Forms.TextBox()
+		Me.txtPeso = New System.Windows.Forms.TextBox()
+		Me.txtPrecio = New System.Windows.Forms.TextBox()
+		Me.dtpCaducidad = New System.Windows.Forms.DateTimePicker()
 		Me.txtNuevaSubfamilia = New System.Windows.Forms.TextBox()
 		Me.txtNuevaFamilia = New System.Windows.Forms.TextBox()
 		Me.cbFamilia = New System.Windows.Forms.ComboBox()
@@ -36,61 +36,72 @@ Partial Class FrmArticulo
 		Me.cbAgregarFam = New System.Windows.Forms.CheckBox()
 		Me.cbAgregarSub = New System.Windows.Forms.CheckBox()
 		Me.btnAgregarProducto = New System.Windows.Forms.Button()
+		Me.lblNombre = New System.Windows.Forms.Label()
+		Me.lblIdprovedor = New System.Windows.Forms.Label()
+		Me.lblCodigo = New System.Windows.Forms.Label()
+		Me.lblDescripcion = New System.Windows.Forms.Label()
+		Me.lblPeso = New System.Windows.Forms.Label()
+		Me.lblCaducidad = New System.Windows.Forms.Label()
+		Me.lblPrecio = New System.Windows.Forms.Label()
+		Me.lblFamilia = New System.Windows.Forms.Label()
+		Me.lblSubfamilia = New System.Windows.Forms.Label()
+		Me.btnBuscarProveedor = New System.Windows.Forms.Button()
 		Me.SuspendLayout()
 		'
-		'TextBox1
+		'txtNombre
 		'
-		Me.TextBox1.Location = New System.Drawing.Point(13, 13)
-		Me.TextBox1.Name = "TextBox1"
-		Me.TextBox1.Size = New System.Drawing.Size(199, 20)
-		Me.TextBox1.TabIndex = 0
+		Me.txtNombre.Location = New System.Drawing.Point(122, 12)
+		Me.txtNombre.Name = "txtNombre"
+		Me.txtNombre.Size = New System.Drawing.Size(200, 20)
+		Me.txtNombre.TabIndex = 0
 		'
-		'TextBox2
+		'txtProveedor
 		'
-		Me.TextBox2.Location = New System.Drawing.Point(13, 39)
-		Me.TextBox2.Name = "TextBox2"
-		Me.TextBox2.Size = New System.Drawing.Size(199, 20)
-		Me.TextBox2.TabIndex = 1
+		Me.txtProveedor.Location = New System.Drawing.Point(122, 38)
+		Me.txtProveedor.Name = "txtProveedor"
+		Me.txtProveedor.Size = New System.Drawing.Size(200, 20)
+		Me.txtProveedor.TabIndex = 1
 		'
-		'TextBox3
+		'txtCodigo
 		'
-		Me.TextBox3.Location = New System.Drawing.Point(12, 65)
-		Me.TextBox3.Name = "TextBox3"
-		Me.TextBox3.Size = New System.Drawing.Size(200, 20)
-		Me.TextBox3.TabIndex = 2
+		Me.txtCodigo.Location = New System.Drawing.Point(122, 64)
+		Me.txtCodigo.Name = "txtCodigo"
+		Me.txtCodigo.Size = New System.Drawing.Size(200, 20)
+		Me.txtCodigo.TabIndex = 2
 		'
-		'TextBox4
+		'txtDescripcion
 		'
-		Me.TextBox4.Location = New System.Drawing.Point(12, 91)
-		Me.TextBox4.Name = "TextBox4"
-		Me.TextBox4.Size = New System.Drawing.Size(200, 20)
-		Me.TextBox4.TabIndex = 3
+		Me.txtDescripcion.Location = New System.Drawing.Point(122, 222)
+		Me.txtDescripcion.Name = "txtDescripcion"
+		Me.txtDescripcion.Size = New System.Drawing.Size(200, 20)
+		Me.txtDescripcion.TabIndex = 3
 		'
-		'TextBox5
+		'txtPeso
 		'
-		Me.TextBox5.Location = New System.Drawing.Point(12, 117)
-		Me.TextBox5.Name = "TextBox5"
-		Me.TextBox5.Size = New System.Drawing.Size(200, 20)
-		Me.TextBox5.TabIndex = 4
+		Me.txtPeso.Location = New System.Drawing.Point(122, 90)
+		Me.txtPeso.Name = "txtPeso"
+		Me.txtPeso.Size = New System.Drawing.Size(200, 20)
+		Me.txtPeso.TabIndex = 4
 		'
-		'TextBox6
+		'txtPrecio
 		'
-		Me.TextBox6.Location = New System.Drawing.Point(12, 170)
-		Me.TextBox6.Name = "TextBox6"
-		Me.TextBox6.Size = New System.Drawing.Size(200, 20)
-		Me.TextBox6.TabIndex = 5
+		Me.txtPrecio.Location = New System.Drawing.Point(122, 142)
+		Me.txtPrecio.Name = "txtPrecio"
+		Me.txtPrecio.Size = New System.Drawing.Size(200, 20)
+		Me.txtPrecio.TabIndex = 5
 		'
-		'DateTimePicker1
+		'dtpCaducidad
 		'
-		Me.DateTimePicker1.Location = New System.Drawing.Point(12, 144)
-		Me.DateTimePicker1.Name = "DateTimePicker1"
-		Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
-		Me.DateTimePicker1.TabIndex = 6
+		Me.dtpCaducidad.Location = New System.Drawing.Point(122, 116)
+		Me.dtpCaducidad.MinDate = New Date(1755, 7, 17, 0, 0, 0, 0)
+		Me.dtpCaducidad.Name = "dtpCaducidad"
+		Me.dtpCaducidad.Size = New System.Drawing.Size(200, 20)
+		Me.dtpCaducidad.TabIndex = 6
 		'
 		'txtNuevaSubfamilia
 		'
 		Me.txtNuevaSubfamilia.Enabled = False
-		Me.txtNuevaSubfamilia.Location = New System.Drawing.Point(13, 326)
+		Me.txtNuevaSubfamilia.Location = New System.Drawing.Point(122, 325)
 		Me.txtNuevaSubfamilia.Name = "txtNuevaSubfamilia"
 		Me.txtNuevaSubfamilia.Size = New System.Drawing.Size(201, 20)
 		Me.txtNuevaSubfamilia.TabIndex = 7
@@ -98,7 +109,7 @@ Partial Class FrmArticulo
 		'txtNuevaFamilia
 		'
 		Me.txtNuevaFamilia.Enabled = False
-		Me.txtNuevaFamilia.Location = New System.Drawing.Point(13, 273)
+		Me.txtNuevaFamilia.Location = New System.Drawing.Point(122, 271)
 		Me.txtNuevaFamilia.Name = "txtNuevaFamilia"
 		Me.txtNuevaFamilia.Size = New System.Drawing.Size(200, 20)
 		Me.txtNuevaFamilia.TabIndex = 8
@@ -106,7 +117,7 @@ Partial Class FrmArticulo
 		'cbFamilia
 		'
 		Me.cbFamilia.FormattingEnabled = True
-		Me.cbFamilia.Location = New System.Drawing.Point(12, 196)
+		Me.cbFamilia.Location = New System.Drawing.Point(122, 168)
 		Me.cbFamilia.Name = "cbFamilia"
 		Me.cbFamilia.Size = New System.Drawing.Size(200, 21)
 		Me.cbFamilia.TabIndex = 9
@@ -114,7 +125,7 @@ Partial Class FrmArticulo
 		'cbSubfamilia
 		'
 		Me.cbSubfamilia.FormattingEnabled = True
-		Me.cbSubfamilia.Location = New System.Drawing.Point(13, 223)
+		Me.cbSubfamilia.Location = New System.Drawing.Point(122, 195)
 		Me.cbSubfamilia.Name = "cbSubfamilia"
 		Me.cbSubfamilia.Size = New System.Drawing.Size(199, 21)
 		Me.cbSubfamilia.TabIndex = 10
@@ -122,7 +133,7 @@ Partial Class FrmArticulo
 		'cbAgregarFam
 		'
 		Me.cbAgregarFam.AutoSize = True
-		Me.cbAgregarFam.Location = New System.Drawing.Point(13, 250)
+		Me.cbAgregarFam.Location = New System.Drawing.Point(122, 248)
 		Me.cbAgregarFam.Name = "cbAgregarFam"
 		Me.cbAgregarFam.Size = New System.Drawing.Size(152, 17)
 		Me.cbAgregarFam.TabIndex = 12
@@ -132,7 +143,7 @@ Partial Class FrmArticulo
 		'cbAgregarSub
 		'
 		Me.cbAgregarSub.AutoSize = True
-		Me.cbAgregarSub.Location = New System.Drawing.Point(13, 303)
+		Me.cbAgregarSub.Location = New System.Drawing.Point(122, 302)
 		Me.cbAgregarSub.Name = "cbAgregarSub"
 		Me.cbAgregarSub.Size = New System.Drawing.Size(166, 17)
 		Me.cbAgregarSub.TabIndex = 13
@@ -141,18 +152,118 @@ Partial Class FrmArticulo
 		'
 		'btnAgregarProducto
 		'
-		Me.btnAgregarProducto.Location = New System.Drawing.Point(13, 352)
+		Me.btnAgregarProducto.Location = New System.Drawing.Point(122, 351)
 		Me.btnAgregarProducto.Name = "btnAgregarProducto"
 		Me.btnAgregarProducto.Size = New System.Drawing.Size(325, 58)
 		Me.btnAgregarProducto.TabIndex = 14
 		Me.btnAgregarProducto.Text = "Agregar producto"
 		Me.btnAgregarProducto.UseVisualStyleBackColor = True
 		'
+		'lblNombre
+		'
+		Me.lblNombre.AutoSize = True
+		Me.lblNombre.Location = New System.Drawing.Point(13, 19)
+		Me.lblNombre.Name = "lblNombre"
+		Me.lblNombre.Size = New System.Drawing.Size(103, 13)
+		Me.lblNombre.TabIndex = 15
+		Me.lblNombre.Text = "Nombre del artículo "
+		'
+		'lblIdprovedor
+		'
+		Me.lblIdprovedor.AutoSize = True
+		Me.lblIdprovedor.Location = New System.Drawing.Point(12, 45)
+		Me.lblIdprovedor.Name = "lblIdprovedor"
+		Me.lblIdprovedor.Size = New System.Drawing.Size(106, 13)
+		Me.lblIdprovedor.TabIndex = 16
+		Me.lblIdprovedor.Text = "Codigo de proveedor"
+		'
+		'lblCodigo
+		'
+		Me.lblCodigo.AutoSize = True
+		Me.lblCodigo.Location = New System.Drawing.Point(12, 71)
+		Me.lblCodigo.Name = "lblCodigo"
+		Me.lblCodigo.Size = New System.Drawing.Size(96, 13)
+		Me.lblCodigo.TabIndex = 17
+		Me.lblCodigo.Text = "Código del artículo"
+		'
+		'lblDescripcion
+		'
+		Me.lblDescripcion.AutoSize = True
+		Me.lblDescripcion.Location = New System.Drawing.Point(13, 229)
+		Me.lblDescripcion.Name = "lblDescripcion"
+		Me.lblDescripcion.Size = New System.Drawing.Size(63, 13)
+		Me.lblDescripcion.TabIndex = 18
+		Me.lblDescripcion.Text = "Descripción"
+		'
+		'lblPeso
+		'
+		Me.lblPeso.AutoSize = True
+		Me.lblPeso.Location = New System.Drawing.Point(13, 97)
+		Me.lblPeso.Name = "lblPeso"
+		Me.lblPeso.Size = New System.Drawing.Size(87, 13)
+		Me.lblPeso.TabIndex = 19
+		Me.lblPeso.Text = "Peso del artículo"
+		'
+		'lblCaducidad
+		'
+		Me.lblCaducidad.AutoSize = True
+		Me.lblCaducidad.Location = New System.Drawing.Point(13, 125)
+		Me.lblCaducidad.Name = "lblCaducidad"
+		Me.lblCaducidad.Size = New System.Drawing.Size(105, 13)
+		Me.lblCaducidad.TabIndex = 20
+		Me.lblCaducidad.Text = "Fecha de caducidad"
+		'
+		'lblPrecio
+		'
+		Me.lblPrecio.AutoSize = True
+		Me.lblPrecio.Location = New System.Drawing.Point(13, 151)
+		Me.lblPrecio.Name = "lblPrecio"
+		Me.lblPrecio.Size = New System.Drawing.Size(93, 13)
+		Me.lblPrecio.TabIndex = 21
+		Me.lblPrecio.Text = "Precio del artículo"
+		'
+		'lblFamilia
+		'
+		Me.lblFamilia.AutoSize = True
+		Me.lblFamilia.Location = New System.Drawing.Point(13, 176)
+		Me.lblFamilia.Name = "lblFamilia"
+		Me.lblFamilia.Size = New System.Drawing.Size(39, 13)
+		Me.lblFamilia.TabIndex = 22
+		Me.lblFamilia.Text = "Familia"
+		'
+		'lblSubfamilia
+		'
+		Me.lblSubfamilia.AutoSize = True
+		Me.lblSubfamilia.Location = New System.Drawing.Point(13, 204)
+		Me.lblSubfamilia.Name = "lblSubfamilia"
+		Me.lblSubfamilia.Size = New System.Drawing.Size(53, 13)
+		Me.lblSubfamilia.TabIndex = 23
+		Me.lblSubfamilia.Text = "Subfamila"
+		'
+		'btnBuscarProveedor
+		'
+		Me.btnBuscarProveedor.Location = New System.Drawing.Point(328, 35)
+		Me.btnBuscarProveedor.Name = "btnBuscarProveedor"
+		Me.btnBuscarProveedor.Size = New System.Drawing.Size(62, 23)
+		Me.btnBuscarProveedor.TabIndex = 24
+		Me.btnBuscarProveedor.Text = "Buscar"
+		Me.btnBuscarProveedor.UseVisualStyleBackColor = True
+		'
 		'FrmArticulo
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.ClientSize = New System.Drawing.Size(843, 643)
+		Me.Controls.Add(Me.btnBuscarProveedor)
+		Me.Controls.Add(Me.lblSubfamilia)
+		Me.Controls.Add(Me.lblFamilia)
+		Me.Controls.Add(Me.lblPrecio)
+		Me.Controls.Add(Me.lblCaducidad)
+		Me.Controls.Add(Me.lblPeso)
+		Me.Controls.Add(Me.lblDescripcion)
+		Me.Controls.Add(Me.lblCodigo)
+		Me.Controls.Add(Me.lblIdprovedor)
+		Me.Controls.Add(Me.lblNombre)
 		Me.Controls.Add(Me.btnAgregarProducto)
 		Me.Controls.Add(Me.cbAgregarSub)
 		Me.Controls.Add(Me.cbAgregarFam)
@@ -160,13 +271,13 @@ Partial Class FrmArticulo
 		Me.Controls.Add(Me.cbFamilia)
 		Me.Controls.Add(Me.txtNuevaFamilia)
 		Me.Controls.Add(Me.txtNuevaSubfamilia)
-		Me.Controls.Add(Me.DateTimePicker1)
-		Me.Controls.Add(Me.TextBox6)
-		Me.Controls.Add(Me.TextBox5)
-		Me.Controls.Add(Me.TextBox4)
-		Me.Controls.Add(Me.TextBox3)
-		Me.Controls.Add(Me.TextBox2)
-		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.dtpCaducidad)
+		Me.Controls.Add(Me.txtPrecio)
+		Me.Controls.Add(Me.txtPeso)
+		Me.Controls.Add(Me.txtDescripcion)
+		Me.Controls.Add(Me.txtCodigo)
+		Me.Controls.Add(Me.txtProveedor)
+		Me.Controls.Add(Me.txtNombre)
 		Me.Name = "FrmArticulo"
 		Me.Text = "FrmArticulo"
 		Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
@@ -175,13 +286,13 @@ Partial Class FrmArticulo
 
 	End Sub
 
-	Friend WithEvents TextBox1 As TextBox
-	Friend WithEvents TextBox2 As TextBox
-	Friend WithEvents TextBox3 As TextBox
-	Friend WithEvents TextBox4 As TextBox
-	Friend WithEvents TextBox5 As TextBox
-	Friend WithEvents TextBox6 As TextBox
-	Friend WithEvents DateTimePicker1 As DateTimePicker
+	Friend WithEvents txtNombre As TextBox
+	Friend WithEvents txtProveedor As TextBox
+	Friend WithEvents txtCodigo As TextBox
+	Friend WithEvents txtDescripcion As TextBox
+	Friend WithEvents txtPeso As TextBox
+	Friend WithEvents txtPrecio As TextBox
+	Friend WithEvents dtpCaducidad As DateTimePicker
 	Friend WithEvents txtNuevaSubfamilia As TextBox
 	Friend WithEvents txtNuevaFamilia As TextBox
 	Friend WithEvents cbFamilia As ComboBox
@@ -189,4 +300,14 @@ Partial Class FrmArticulo
 	Friend WithEvents cbAgregarFam As CheckBox
 	Friend WithEvents cbAgregarSub As CheckBox
 	Friend WithEvents btnAgregarProducto As Button
+	Friend WithEvents lblNombre As Label
+	Friend WithEvents lblIdprovedor As Label
+	Friend WithEvents lblCodigo As Label
+	Friend WithEvents lblDescripcion As Label
+	Friend WithEvents lblPeso As Label
+	Friend WithEvents lblCaducidad As Label
+	Friend WithEvents lblPrecio As Label
+	Friend WithEvents lblFamilia As Label
+	Friend WithEvents lblSubfamilia As Label
+	Friend WithEvents btnBuscarProveedor As Button
 End Class
--- Proyecto Sistema Bolpa/Formularios/FrmArticulo.vb
+++ Proyecto Sistema Bolpa/Formularios/FrmArticulo.vb
@@ -3,8 +3,11 @@
 		' Esta llamada es exigida por el diseñador.
 		InitializeComponent()
 		Size = New Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height)
+		dtpCaducidad.MinDate = Date.Now
 		' Agregue cualquier inicialización después de la llamada a InitializeComponent().
 	End Sub
+
+
 
 	Private Sub CbAgregarFam_CheckedChanged(sender As Object, e As EventArgs) Handles cbAgregarFam.CheckedChanged
 		If cbAgregarFam.Checked Then
@@ -13,7 +16,6 @@
 			CambiarEstado(2)
 		End If
 	End Sub
-
 	Private Sub CbAgregarSub_CheckedChanged(sender As Object, e As EventArgs) Handles cbAgregarSub.CheckedChanged
 		If cbAgregarSub.Checked Then
 			CambiarEstado(3)
@@ -21,7 +23,6 @@
 			CambiarEstado(4)
 		End If
 	End Sub
-
 	Private Sub CambiarEstado(ByVal Opc As Integer)
 		Select Case (Opc)
 			Case 1
@@ -48,5 +49,119 @@
 				cbAgregarSub.Enabled = True
 		End Select
 	End Sub
+
+	Private Sub TxtPrecio_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPrecio.KeyPress
+		If Char.IsNumber(e.KeyChar) OrElse Char.IsPunctuation(e.KeyChar) And VerificarContenidoPuntos(txtPrecio.Text) Then
+			e.Handled = False
+		Else
+			e.Handled = True
+		End If
+	End Sub
+	Private Sub TxtPeso_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPeso.KeyPress
+		If Char.IsNumber(e.KeyChar) Or Char.IsPunctuation(e.KeyChar) And VerificarContenidoPuntos(txtPrecio.Text) Then
+			e.Handled = False
+		Else
+			e.Handled = True
+		End If
+	End Sub
+	''' <summary>
+	''' Método se encarga se verificar si la cifra tiene un punto
+	''' </summary>
+	''' <param name="Cadena"></param>
+	''' <returns></returns>
+	Private Function VerificarContenidoPuntos(ByVal Cadena As String) As Boolean
+		Try
+			Dim Verificacion As Double = Cadena
+		Catch ex As Exception
+			MsgBox(ex.Message)
+			Return False
+		End Try
+		Return True
+	End Function
+
+	Private Sub TxtNombre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNombre.KeyPress
+		VerificarEntradadas(e)
+	End Sub
+	Private Sub TxtCodigo_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtCodigo.KeyPress
+		VerificarEntradadas(e)
+	End Sub
+	Private Sub TxtNuevaFamilia_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNuevaFamilia.KeyPress
+		VerificarEntradadas(e)
+	End Sub
+	Private Sub TxtNuevaSubfamilia_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNuevaSubfamilia.KeyPress
+		VerificarEntradadas(e)
+	End Sub
+	Private Sub TxtDescripcion_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtDescripcion.KeyPress
+		VerificarEntradadas(e)
+	End Sub
+	Private Sub TxtProveedor_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtProveedor.KeyPress
+		VerificarEntradadas(e)
+	End Sub
+	''' <summary>
+	''' Método se encarga de filtar de tal manera que solo permita caracteres y separadores
+	''' </summary>
+	''' <param name="e"></param>
+	Private Sub VerificarEntradadas(e As KeyPressEventArgs)
+		If Not Char.IsPunctuation(e.KeyChar) OrElse Char.IsSeparator(e.KeyChar) Then
+			e.Handled = False
+		Else
+			e.Handled = True
+		End If
+	End Sub
+
+	Public ReadOnly Property Nombre() As String
+		Get
+			Return txtNombre.Text
+		End Get
+	End Property
+	Public ReadOnly Property CodigoProveedor() As String
+		Get
+			Return txtProveedor.Text
+		End Get
+	End Property
+	Public ReadOnly Property CodigoArticulo() As String
+		Get
+			Return txtCodigo.Text
+		End Get
+	End Property
+	Public ReadOnly Property Peso() As Double
+		Get
+			Return txtPeso.Text
+		End Get
+	End Property
+	Public ReadOnly Property FechaCaducidad() As String
+		Get
+			Return dtpCaducidad.Text
+		End Get
+	End Property
+	Public ReadOnly Property Precio() As Double
+		Get
+			Return txtPrecio.Text
+		End Get
+	End Property
+	Public ReadOnly Property Familia() As String
+		Get
+			If cbAgregarFam.Enabled Then
+				Return cbAgregarFam.Text
+			Else
+				Return cbFamilia.Text
+			End If
+		End Get
+	End Property
+	Public ReadOnly Property Subfamilia() As String
+		Get
+			If cbAgregarSub.Enabled Then
+				Return cbAgregarSub.Text
+			Else
+				Return cbSubfamilia.Text
+			End If
+		End Get
+	End Property
+	Public ReadOnly Property Descripcion() As String
+		Get
+			Return txtDescripcion.Text
+		End Get
+	End Property
+
 
 End Class
