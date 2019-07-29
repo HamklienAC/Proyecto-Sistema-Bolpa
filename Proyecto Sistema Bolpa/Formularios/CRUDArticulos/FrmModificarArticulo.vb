56835af98e75d8a9db2429437d49365556274ddf
HamklienAC <49457002+HamklienAC@users.noreply.github.com>
2019-07-28 20:05:43 -0600

    ...

---
+++ Proyecto Sistema Bolpa/Formularios/CRUDArticulos/FrmModificarArticulo.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmModificarArticulo
+
+End Class
--- Proyecto Sistema Bolpa/My Project/Application.Designer.vb
+++ Proyecto Sistema Bolpa/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto_Sistema_Bolpa.FrmBuscarArticulo
+            Me.MainForm = Global.Proyecto_Sistema_Bolpa.FrmModificarArticulo
         End Sub
     End Class
 End Namespace
