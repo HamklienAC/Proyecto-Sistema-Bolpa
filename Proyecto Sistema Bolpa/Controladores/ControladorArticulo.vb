963271d78157b90e1b693d235e2e40ebcad233e8
HamklienAC <49457002+HamklienAC@users.noreply.github.com>
2019-07-30 21:32:35 -0600

    Nah

--- Proyecto Sistema Bolpa/Controladores/ControladorArticulo.vb
+++ Proyecto Sistema Bolpa/Controladores/ControladorArticulo.vb
@@ -1,7 +1,7 @@
 ﻿Public Class ControladorArticulo
     Dim articulo As clsArticulo
-
-    Public Sub AgregarArticulo(datos() As String)
+	'Hola mundo
+	Public Sub AgregarArticulo(datos() As String)
         articulo = New clsArticulo(0, datos(0), datos(1), datos(2), datos(3), datos(4), datos(5), datos(6), datos(7), datos(8))
 
         Using BD As New Proyecto_Sistema_Bolpa.ProyectoBD
